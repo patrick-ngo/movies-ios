b31505ff4fa9433d484fd9d65ff80788a9a84eb6
Patrick Ngo <patrick.ngo@hotmail.com>
2017-11-10 22:21:20 +0800

    Initial MovieListings as first screen

--- movies-ios/AppDelegate.swift
+++ movies-ios/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
         
-        self.window?.rootViewController = UINavigationController(rootViewController: UIViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: MovieListings())
         
         // Override point for customization after application launch.
         return true
---
+++ movies-ios/ViewControllers/MovieListingsVC.swift
@@ -0,0 +1,88 @@
+//
+//  MovieListingsVC.swift
+//  movies-ios
+//
+//  Created by Patrick Ngo on 11/10/17.
+//  Copyright © 2017 patrickngo. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class MovieListingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: - Views -
+    
+    private lazy var tableView : UITableView = {
+        
+        let tv = UITableView(frame: CGRect.zero, style: .plain)
+        tv.separatorStyle = .none
+        
+        tv.delegate = self
+        tv.dataSource = self
+        
+        //cell registration
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        
+        return tv
+    }()
+    
+    private let refreshControl:UIRefreshControl = {
+        let rc = UIRefreshControl()
+        return rc
+    }()
+    
+    //MARK: - Init -
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setupNavBar()
+        self.setupViews()
+    }
+    
+    func setupNavBar() {
+        
+    }
+    
+     func setupViews() {
+
+        self.tableView.addSubview(self.refreshControl)
+        self.view.addSubview(self.tableView)
+        
+        self.tableView.snp.makeConstraints { (make) in
+            make.top.left.right.bottom.equalTo(0)
+        }
+    }
+
+    //MARK: - TableView Datasource -
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self))
+        cell?.backgroundColor = .blue
+        return cell!
+    }
+    
+    
+    //MARK: - TableView Delegate -
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //do nothing
+    }
+    
+
+}
+
