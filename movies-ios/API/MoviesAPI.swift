050b20746c9630c5e43cd8c00f2dfd0f185735db
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-15 01:24:35 +0800

    Add retrieveMovie method to retrieve single movie by id Always set current date as primary_release_date when loading movie list

--- movies-ios/API/MoviesAPI.swift
+++ movies-ios/API/MoviesAPI.swift
@@ -8,38 +8,48 @@
 
 import Alamofire
 
-
-
 typealias JSONCompletionBlock = ( [String:Any]? ,_ error: Error?) -> Void
 typealias DataCompletionBlock = ( Data? ,_ error: Error?) -> Void
 
 class MoviesAPI
 {
     static let API_KEY = "e4a3bc287b929e12897dd730b1b153e9"
-    
     static let BASE_URL = "https://api.themoviedb.org/3/"
-    
     static let BASE_URL_IMAGES_LOW = "https://image.tmdb.org/t/p/w185"
     static let BASE_URL_IMAGES_HIGH = "https://image.tmdb.org/t/p/w500"
     
     static let shared = MoviesAPI()
     
+    let dateFormatter = DateFormatter()
+    
+    init() {
+        self.dateFormatter.dateFormat = "yyyy-MM-dd"
+    }
+    
     //MARK: - API methods -
     
-    //Retrieve movies
+    // Retrieve list of movies
     func retrieveMovies(page: Int = 1, completionHandler: @escaping DataCompletionBlock) {
-        
         let endPoint = "discover/movie"
         let url = URL(string: "\(MoviesAPI.BASE_URL)\(endPoint)")!
-        
-        
+        let currentDate = self.dateFormatter.string(from: Date())
         let parameters: Parameters = ["page": page,
                                       "api_key": MoviesAPI.API_KEY,
                                       "sort_by": "release_date.desc",
-                                      "primary_release_date.lte": "2016-12-31"]
+                                      "primary_release_date.lte": currentDate]
         
         Alamofire.request(url, parameters: parameters).responseJSON { response in
-            
+            completionHandler(response.data, response.error)
+        }
+    }
+    
+    // Retrieve single movie by id
+    func retrieveMovie(byId movieId: Int, completionHandler: @escaping DataCompletionBlock) {
+        let endPoint = "movie"
+        let url = URL(string: "\(MoviesAPI.BASE_URL)\(endPoint)/\(movieId)")!
+        let parameters: Parameters = ["api_key": MoviesAPI.API_KEY]
+        
+        Alamofire.request(url, parameters: parameters).responseJSON { response in
             completionHandler(response.data, response.error)
         }
     }
